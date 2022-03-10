8bcdccba2eb3a32b3057d5d390a3620baf369f02
Anna-AK <95749542+Anna-AK@users.noreply.github.com>
2022-03-10 13:49:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5087845858:AAG8qP_Bpnf6QBGsBj7lI9PMf5EJYoNppj8"
+
+valuts = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'йена': 'JPY',
+    'фунт': 'GBP'
+}
